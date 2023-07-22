be6c352a457e6354167faee446f7ddb57119c91e
rasoolasn <rasoolasn@gmail.com>
2023-07-22 11:21:17 +0530

    shipping updated

--- 08-Shipping.sh
+++ 08-Shipping.sh
@@ -3,6 +3,7 @@ yum install maven -y
 useradd roboshop
 
 rm -rf /app
+
 mkdir /app
 
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
@@ -13,8 +14,10 @@ cd /app
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 
-systemctl daemon-reload
+yum install mysql -y
+mysql -h mysql.ndevops.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
 
+systemctl daemon-reload
 systemctl enable shipping
 systemctl restart shipping
 
