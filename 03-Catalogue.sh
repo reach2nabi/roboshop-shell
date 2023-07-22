dfd0f92ccfa4994d78a2e1cb70fb1076af56abbb
rasoolasn <rasoolasn@gmail.com>
2023-07-22 17:18:41 +0530

    new changes

--- 03-Catalogue.sh
+++ 03-Catalogue.sh
@@ -1,47 +1,47 @@
 
 echo -e "\e[31m>>>>>>>>>> Nodejs File Content<<<<<<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service
-cp mongo.conf /etc/yum.repos.d/mongo.repo
+cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+cp mongo.conf /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
 echo -e "\e[32m>>>>>>>>>> Nodejs File Content<<<<<<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
 echo -e "\e[33m>>>>>>>>>> Install Nodejs File Content<<<<<<<<<<\e[0m"
-yum install nodejs -y
+yum install nodejs -y &>>/tmp/roboshop.log
 
 echo -e "\e[34m>>>>>>>>>> Useradd to Service <<<<<<<<<<\e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[35m>>>>>>>>>> Remove previous file from the application  <<<<<<<<<<\e[0m"
 
-#rm -rf /app
+rm -rf /app &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>>>> Create Directory  <<<<<<<<<<\e[0m"
-mkdir /app
+mkdir /app &>>/tmp/roboshop.log
 
 echo -e "\e[31m>>>>>>>>>> Catalogue Source File   <<<<<<<<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
 
 echo -e "\e[32m>>>>>>>>>> Extract Source File   <<<<<<<<<<\e[0m"
-cd /app
-unzip /tmp/catalogue.zip
+cd /app &>>/tmp/roboshop.log
+unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
 
 echo -e "\e[33m>>>>>>>>>> Move to Directory   <<<<<<<<<<\e[0m"
 
-cd /app
+cd /app &>>/tmp/roboshop.log
 
 echo -e "\e[34m>>>>>>>>>> Install dependent files  <<<<<<<<<<\e[0m"
 
-npm install
+npm install &>>/tmp/roboshop.log
 
 echo -e "\e[35m>>>>>>>>>> Install mongodb  <<<<<<<<<<\e[0m"
-yum install mongodb-org-shell -y
+yum install mongodb-org-shell -y &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>>>> Create mongodb Schema   <<<<<<<<<<\e[0m"
-mongo --host mongodb.ndevops.online </app/schema/catalogue.js
+mongo --host mongodb.ndevops.online </app/schema/catalogue.js &>>/tmp/roboshop.log
 
 echo -e "\e[31m>>>>>>>>>> Enable an Restart   <<<<<<<<<<\e[0m"
 
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl restart catalogue
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable catalogue &>>/tmp/roboshop.log
+systemctl restart catalogue &>>/tmp/roboshop.log
