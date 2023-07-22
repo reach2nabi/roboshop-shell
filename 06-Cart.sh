7ed600894c99a036e2cba4f394f1d561febfd326
rasoolasn <rasoolasn@gmail.com>
2023-07-22 08:34:33 +0530

    remove file

--- 06-Cart.sh
+++ 06-Cart.sh
@@ -5,11 +5,11 @@ yum install nodejs -y
 useradd roboshop
 
 rm -rf /app
-
 mkdir /app
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
 unzip /tmp/cart.zip
+
 cd /app
 npm install
 
