ada5f8b04b3561a053b6fdde1298937abb5479fb
rasoolasn <rasoolasn@gmail.com>
2023-07-22 17:03:22 +0530

    new changes

--- 03-Catalogue.sh
+++ 03-Catalogue.sh
@@ -1,46 +1,46 @@
 
-echo  ">>>>>>>>>> Nodejs File Content<<<<<<<<<<"
+echo -e "\e[31>>>>>>>>>> Nodejs File Content<<<<<<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
 cp mongo.conf /etc/yum.repos.d/mongo.repo
 
-echo  ">>>>>>>>>> Nodejs File Content<<<<<<<<<<"
+echo -e "\e[32>>>>>>>>>> Nodejs File Content<<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo  ">>>>>>>>>> Install Nodejs File Content<<<<<<<<<<"
+echo -e "\e[33>>>>>>>>>> Install Nodejs File Content<<<<<<<<<<\e[0m"
 yum install nodejs -y
 
-echo  ">>>>>>>>>> Useradd to Service <<<<<<<<<<"
+echo  "\e[34>>>>>>>>>> Useradd to Service <<<<<<<<<<\e[0m"
 useradd roboshop
 
-echo  ">>>>>>>>>> Remove previous file from the application  <<<<<<<<<<"
+echo  "\e[35>>>>>>>>>> Remove previous file from the application  <<<<<<<<<<\e[0m"
 
 #rm -rf /app
 
-echo  ">>>>>>>>>> Create Directory  <<<<<<<<<<"
+echo  "\e[36>>>>>>>>>> Create Directory  <<<<<<<<<<\e[0m"
 mkdir /app
 
-echo  ">>>>>>>>>> Catalogue Source File   <<<<<<<<<<"
+echo  "\e[31>>>>>>>>>> Catalogue Source File   <<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 
-echo  ">>>>>>>>>> Extract Source File   <<<<<<<<<<"
+echo  "\e[32>>>>>>>>>> Extract Source File   <<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/catalogue.zip
 
-echo  ">>>>>>>>>> Move to Directory   <<<<<<<<<<"
+echo  "\e[33>>>>>>>>>> Move to Directory   <<<<<<<<<<\e[0m"
 
 cd /app
 
-echo  ">>>>>>>>>> Install dependent files  <<<<<<<<<<"
+echo  "\e[34>>>>>>>>>> Install dependent files  <<<<<<<<<<\e[0m"
 
 npm install
 
-echo  ">>>>>>>>>> Install mongodb  <<<<<<<<<<"
+echo  "\e[35>>>>>>>>>> Install mongodb  <<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
 
-echo  ">>>>>>>>>> Create mongodb Schema   <<<<<<<<<<"
+echo  "\e[36>>>>>>>>>> Create mongodb Schema   <<<<<<<<<<\e[0m"
 mongo --host mongodb.ndevops.online </app/schema/catalogue.js
 
-echo  ">>>>>>>>>> Enable an Restart   <<<<<<<<<<"
+echo  "\e[31>>>>>>>>>> Enable an Restart   <<<<<<<<<<\e[0m"
 
 systemctl daemon-reload
 systemctl enable catalogue
