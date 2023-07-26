b0fa6b20cb25c079e0564408396d936cf5dc5278
rasoolasn <rasoolasn@gmail.com>
2023-07-26 21:26:00 +0530

    if

--- 07-MySQL.sh
+++ 07-MySQL.sh
@@ -1,6 +1,6 @@
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 mysql_password=$1
-if [ -z "{mysql_password}" ]; then
+if [ -z "${mysql_password}" ]; then
   echo Input password is missing
   exit 1
 fi
@@ -10,5 +10,5 @@ yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl start mysqld
 
-mysql_secure_installation --set-root-pass {mysql_password}
+mysql_secure_installation --set-root-pass ${mysql_password}
 #RoboShop@1
