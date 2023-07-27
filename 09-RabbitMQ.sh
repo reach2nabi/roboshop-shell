a809f39f07d2a430bbf0144064a630a944c3fb8e
rasoolasn <rasoolasn@gmail.com>
2023-07-27 06:48:44 +0530

    if

--- 09-RabbitMQ.sh
+++ 09-RabbitMQ.sh
@@ -1,12 +1,14 @@
+echo -e "\e[34m>>>>>>>>>> Installing Service <<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
 
 rabbitmq_password=$1
-if [-z "${rabbitmq_password}"]; then
+if [ -z "${rabbitmq_password}" ]; then
   echo Input Password is Missing
   exit 1
-if
+fi
 
+echo -e "\e[34m>>>>>>>>>> Installing Rabbitmq Service <<<<<<<<<<\e[0m"
 yum install rabbitmq-server -y
 
 systemctl enable rabbitmq-server
