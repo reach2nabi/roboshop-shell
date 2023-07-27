e4acb88a482b3b435981661b8876bbbcdd361920
rasoolasn <rasoolasn@gmail.com>
2023-07-27 06:38:29 +0530

    if

--- common.sh
+++ common.sh
@@ -2,9 +2,10 @@ log=/tmp/roboshop.log
 
 func_exit_status(){
   if [ $? -eq 0 ]; then
-      echo -e "\e[31m>>>>> FAILURE  <<<<<<\e[0m"
+
+      echo -e "\e[32m>SUCCESS\e[0m"
     else
-      echo -e "\e[32m>>>>> SUCCESS <<<<<<\e[0m"
+      echo -e "\e[31mFAILURE<\e[0m"
   fi
 }
 
